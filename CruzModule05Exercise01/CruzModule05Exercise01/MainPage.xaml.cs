059d41777d2d822f334d70a4915a10f4ad9292ef
theredhood427 <bangsil.ronrusselle@auf.edu.ph>
2023-04-26 19:02:34 +0800

    first commit

---
+++ CruzModule05Exercise01/CruzModule05Exercise01/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CruzModule05Exercise01
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void SubmitButton_Clicked(object sender, EventArgs e)
+        {
+            string enteredText = textInput.Text;
+            TimeSpan selectedTime = timePicker.Time;
+
+            string message = $"Entered Text: {enteredText}\nSelected Time: {selectedTime}";
+            DisplayAlert("Form Data", message, "OK");
+        }
+    }
+}
